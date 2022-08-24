afa5c6ec4bc342d7e8846ddf9087055aa32f640b
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-24 11:48:22 +0700

    fix: 🐛 TODO-55 Fix API update task to id param type

--- src/task/dto/update-task-dto.ts
+++ src/task/dto/update-task-dto.ts
@@ -3,10 +3,6 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateTaskDto {
   @ApiProperty()
-  @IsString()
-  id: string;
-
-  @ApiProperty()
   @IsOptional()
   taskName: string;
 }
