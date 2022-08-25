7cdd5dcaaf05ce10a616d6cbb520b22cfc0cb9d0
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-25 21:15:09 +0700

    fix: 🐛 TODO-56: Remove Validation MinLength, MaxLength

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,36 +1,20 @@
 import {
   IsString,
-  MinLength,
-  MaxLength,
   IsUUID,
   IsNumber,
 } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateTaskDto {
-  @ApiProperty({
-    description: "Task name must be between 5 to 100 character",
-    minLength: 5,
-    maxLength: 100,
-  })
-  @MinLength(5, {
-    message: "Task name must at least 5 character",
-  })
-  @MaxLength(100, {
-    message: "Task name cannot be exceed 100 character",
-  })
+  @ApiProperty()
   @IsString()
   taskName: string;
 
-  @ApiProperty({
-    description: "UserId must be UUID and required"
-  })
+  @ApiProperty()
   @IsUUID()
   userId: string;
 
-  @ApiProperty({
-    description: "todolistId must be number and required"
-  })
+  @ApiProperty()
   @IsNumber()
   todolistId: number;
 }
