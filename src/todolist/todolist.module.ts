dd012ff125561bdc1a53085e75a9dfa6c7176ffa
developerAbcSoftware <113164901+developerAbcSoftware@users.noreply.github.com>
2022-09-09 21:33:16 +0700

    find first recor uuid unused

--- src/todolist/todolist.module.ts
+++ src/todolist/todolist.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Todolist } from "./entities/todolist.entity";
 import { TaskService } from 'src/task/task.service';
 import { Task } from 'src/task/entities/task.entity';
+import { UuidstorageService } from "src/uuidstorage/uuidstorage.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todolist, Task])],
   controllers: [TodolistController],
-  providers: [TodolistService, TaskService],
+  providers: [TodolistService, TaskService, UuidstorageService],
 })
 export class TodolistModule {}
