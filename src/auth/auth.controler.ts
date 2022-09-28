0746f93ee35b49825c7781297a363e53a0411b79
lamminhthien <lamminhthien02012000@gmail.com>
2022-09-28 08:53:58 +0700

    attach email ok

--- src/auth/auth.controler.ts
+++ src/auth/auth.controler.ts
@@ -1,9 +1,11 @@
 import {Body, Catch, Controller, Get, Post, Req, UseGuards} from '@nestjs/common';
 import {ApiBearerAuth, ApiTags} from '@nestjs/swagger';
 import {CreateUserDto} from 'src/users/dtos/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 import extractHeader from 'src/utils/extract-header';
 import { EntityNotFoundError, QueryFailedError } from 'typeorm';
 import {AuthService} from './auth.service';
+import { EmailDto } from './dto/email.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @ApiTags('Auth User (Created Token)')
@@ -11,7 +13,7 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 @Controller('auth')
 @Catch(QueryFailedError, EntityNotFoundError)
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private userService: UsersService) {}
   @Post('/login')
   async checkUserLogin(@Body() userDto: CreateUserDto) {
     console.log(`😀Created Access Token for userName: ${userDto.userName} `);
@@ -27,10 +29,15 @@ export class AuthController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('/attach_email;')
-  async abc(@Req() request: any) {
+  @Post('/attach_email;')
+  async abc(@Req() request: any, @Body() emailDto: EmailDto) {
     console.log('😀Decode User Info from Access Token');
     const {userName,userId} = extractHeader(request);
-    return {userName,userId}
+    // return {
+    //   "userName" : userName,
+    //   "userId" : userId,
+    //   "emailDto" : emailDto.email
+    // }
+    return this.userService.attachEmail(emailDto.email,userId);
   }
 }
