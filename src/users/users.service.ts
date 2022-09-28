9b73459c3ebd4b30e96055aec314a5632bbe8646
lamminhthien <lamminhthien02012000@gmail.com>
2022-09-28 16:19:19 +0700

    🐛fix: Prevent email duplicate in more than 2 account

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
-import { BadRequestException, Injectable, NotAcceptableException} from '@nestjs/common';
+import {BadRequestException, Injectable, NotAcceptableException} from '@nestjs/common';
 import {Repository} from 'typeorm';
 import {InjectRepository} from '@nestjs/typeorm';
 import {User} from './entities/user.entity';
-import { uuid } from 'uuidv4';
+import {uuid} from 'uuidv4';
 
 @Injectable()
 export class UsersService {
@@ -10,9 +10,9 @@ export class UsersService {
 
   async validUserId(id: string) {
     // Check if userId existed
-    const userIdExisted = await this.repo.count({id:id});
-    if (userIdExisted >= 1) return false
-    return true
+    const userIdExisted = await this.repo.count({id: id});
+    if (userIdExisted >= 1) return false;
+    return true;
   }
 
   async findAll(): Promise<User[]> {
@@ -22,12 +22,12 @@ export class UsersService {
   async create(userName: string): Promise<User> {
     // create userId
     let userId = uuid();
-    while (await this.validUserId(userId)==false) {
+    while ((await this.validUserId(userId)) == false) {
       userId = uuid();
     }
 
     if (userName.trim().length !== 0) {
-      const user = this.repo.create({userName:userName,id:userId});
+      const user = this.repo.create({userName: userName, id: userId});
       return this.repo.save(user);
     } else throw new NotAcceptableException('Username must have 1 character');
   }
@@ -38,8 +38,8 @@ export class UsersService {
   }
 
   async findUserByEmail(email: string) {
-      const firstUser = await this.repo.findOne({email:email});
-      return firstUser;
+    const firstUser = await this.repo.findOne({email: email});
+    return firstUser;
   }
 
   async findUserByName(userName: string) {
@@ -51,13 +51,15 @@ export class UsersService {
   }
 
   async attachEmail(email: string, id: string) {
-    const user = await this.repo.findOne({id:id})
+    const emailExisted = await this.repo.find({email: email});
+    const currentUser = await this.repo.findOne({id:id});
     // If user already have email block this function
-    if (user.email) {
-      throw new BadRequestException('🥵 This user already have email linked')
-    } else
+    if (emailExisted.length > 0) {
+      throw new BadRequestException('🥵 This user already have email linked');
+    } else {
+      currentUser.email = email;
+      return await this.repo.save(currentUser);
+    }
     // If user doesn't have email
-    user.email = email;
-    return await this.repo.save(user);
   }
 }
