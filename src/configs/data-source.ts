741f09303008100a0d33a48935761b5bcd3425c0
Xuân Huy <itnxhuy1806@gmail.com>
2022-10-20 08:30:35 +0700

    Setup-Migration

---
+++ src/configs/data-source.ts
@@ -0,0 +1,20 @@
+import { Pool } from 'src/database/pool/pool.entity';
+import { Task } from 'src/database/task/task.entity';
+import { Todolist } from 'src/database/todolist/todolist.entity';
+import { User } from 'src/database/user/user.entity';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const config: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5436,
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'postgres',
+  schema: process.env.DATABASE_SCHEMA,
+  entities: [Task, User, Todolist, Pool],
+  migrations: ['src/migrations/*'],
+};
+console.log(config);
+const postgresDataSource = new DataSource(config);
+export default postgresDataSource;
