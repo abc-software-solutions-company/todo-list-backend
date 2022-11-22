463ddc55257c5f25ce07166655251eecf65239c1
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-22 23:07:17 +0700

    apply datadog tracing to nestjs (#81)

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './utils/all-exception.filter';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
+import './ddtrace/tracting';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.setGlobalPrefix('api');
-
+  app.useLogger(app.get(PinoLogger));
   const configService = app.get(ConfigService);
   const config = new DocumentBuilder().setTitle('Todo List API').setVersion('').addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, config);
