470e3b4007ac5fe193ef9fd9fd26ecd5c9b26922
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-08 22:37:00 +0700

    Add test about add user to member list

---
+++ src/database/todolist-user/index.spec.ts
@@ -0,0 +1,44 @@
+import { TestingModule } from '@nestjs/testing';
+import { testHelper } from 'src/utils/testHelper';
+import { UserService } from '../user/index.service';
+import { PoolService } from '../pool/index.service';
+import { TodolistService } from '../todolist/index.service';
+import { TodolistUserService } from './index.service';
+
+describe('TodolistService', () => {
+  let todolistService: TodolistService;
+  let userService: UserService;
+  let poolService: PoolService;
+  let todolistUserService: TodolistUserService;
+
+  let moduleRef: TestingModule;
+
+  beforeEach(async () => {
+    moduleRef = await testHelper();
+    todolistService = moduleRef.get<TodolistService>(TodolistService);
+    userService = moduleRef.get<UserService>(UserService);
+    poolService = moduleRef.get<PoolService>(PoolService);
+    todolistUserService = moduleRef.get<TodolistUserService>(TodolistUserService);
+
+    await poolService.generate(30);
+  });
+  afterEach(async () => {
+    await moduleRef.close();
+  });
+
+  describe('Todolist Use Service', () => {
+    it('Create list and add one user to this list', async () => {
+      const name = 'Linh bé nhỏ';
+      const { id: userId } = await userService.create({
+        email: 'lamminhthien02012000@gmail.com',
+        name,
+      });
+      const { id: todolistId } = await todolistService.create({ name: 'List của Thiện', userId: userId });
+      await todolistUserService.set({ todolistId, ids: [userId] });
+      const response = await todolistUserService.repository.findOne({ where: { userId, isActive: true, todolistId } });
+      expect(response.todolistId).toEqual(todolistId);
+      expect(response.userId).toEqual(userId);
+      expect(response.isActive).toEqual(true);
+    });
+  });
+});
