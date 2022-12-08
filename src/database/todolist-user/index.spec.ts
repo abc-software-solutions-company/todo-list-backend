4993e68ca5e9e7e400869ad5768947bc40745f3f
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-08 22:45:48 +0700

    add test add a lot of user to member on list

--- src/database/todolist-user/index.spec.ts
+++ src/database/todolist-user/index.spec.ts
@@ -26,7 +26,7 @@ describe('TodolistService', () => {
     await moduleRef.close();
   });
 
-  describe('Todolist Use Service', () => {
+  describe('Todolist Use Service (Add member to list)', () => {
     it('Create list and add one user to this list', async () => {
       const name = 'Linh bé nhỏ';
       const { id: userId } = await userService.create({
@@ -39,6 +39,51 @@ describe('TodolistService', () => {
       expect(response.todolistId).toEqual(todolistId);
       expect(response.userId).toEqual(userId);
       expect(response.isActive).toEqual(true);
+    });
+
+    it('Create list and add a lot of user to this list', async () => {
+      const name = 'Linh bé nhỏ';
+      const { id: userId } = await userService.create({
+        email: 'lamminhthien02012000@gmail.com',
+        name,
+      });
+      const { id: userId2 } = await userService.create({
+        email: 'johnathan19082022@gmail.com',
+        name: 'Johnathan',
+      });
+      const { id: userId3 } = await userService.create({
+        email: 'mrgoodtea_vietnam@gmail.com',
+        name: 'MrGoodTea',
+      });
+      const { id: todolistId } = await todolistService.create({ name: 'List của Thiện', userId: userId });
+      await todolistUserService.set({ todolistId, ids: [userId, userId2, userId3] });
+      const response = await todolistUserService.repository.find({ where: { isActive: true, todolistId } });
+      expect(response.length).toEqual(3);
+    });
+
+    it('Create list and add a lot of user to this list and then remove one user', async () => {
+      const name = 'Linh bé nhỏ';
+      const { id: userId } = await userService.create({
+        email: 'lamminhthien02012000@gmail.com',
+        name,
+      });
+      const { id: userId2 } = await userService.create({
+        email: 'johnathan19082022@gmail.com',
+        name: 'Johnathan',
+      });
+      const { id: userId3 } = await userService.create({
+        email: 'mrgoodtea_vietnam@gmail.com',
+        name: 'MrGoodTea',
+      });
+      const { id: todolistId } = await todolistService.create({ name: 'List của Thiện', userId: userId });
+      await todolistUserService.set({ todolistId, ids: [userId, userId2, userId3] });
+      const before = await todolistUserService.repository.find({ where: { isActive: true, todolistId } });
+      expect(before.length).toEqual(3);
+
+      // Sau đó xóa bớt 1 người ra khỏi list
+      await todolistUserService.set({ todolistId, ids: [userId, userId2] });
+      const after = await todolistUserService.repository.find({ where: { isActive: true, todolistId } });
+      expect(after.length).toEqual(2);
     });
   });
 });
