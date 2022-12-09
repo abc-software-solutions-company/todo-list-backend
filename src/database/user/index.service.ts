900dc4f85bb911e22efc8ce3618b99c018980242
Phuoc Huu <110363738+phuocvuive09@users.noreply.github.com>
2022-12-09 13:18:37 +0700

    🐛 TODOOY-18: Feature rename user at BE (#102)
    * TODOOY-18: Feature rename user at BE
    
    * Refactor some codes

--- src/database/user/index.service.ts
+++ src/database/user/index.service.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { IsNull, Not, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './index.entity';
 import { v4 } from 'uuid';
+import { IUserUpdate } from './index.type';
+import { defineAll, defineAny } from 'src/utils/function';
 
 interface ICreate {
   name: string;
@@ -21,5 +23,22 @@ export class UserService {
     const id = v4();
     const user = this.repository.create({ name, id, email });
     return this.repository.save(user);
+  }
+
+  async update({ id, name }: IUserUpdate) {
+    if (!defineAll(id, name)) throw new BadRequestException('User update Err param');
+
+    const user = await this.repository.findOneBy({ id });
+
+    if (defineAny(name)) {
+      if (name) {
+        if (!name.trim()) throw new BadRequestException('Empty name');
+        user.name = name;
+      }
+
+      await this.repository.save(user);
+    }
+
+    return user;
   }
 }
