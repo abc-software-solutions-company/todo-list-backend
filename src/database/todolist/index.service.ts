8f289bbe34e8db47cf3b2740a1211e60e17767cd
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-05 15:44:47 +0700

    KANBAN: Get task for column order by indexColumn (#120)
    * create seperate api for kanban
    
    * now can we update index on other column

--- src/database/todolist/index.service.ts
+++ src/database/todolist/index.service.ts
@@ -169,8 +169,58 @@ export class TodolistService {
     const taskRecords = this.task.repository.find({
       select: ['id', 'name', 'isDone', 'statusId', 'index', 'priority', 'createdDate'],
       where: { todolistId: id, isActive: true },
-      relations: { assignees: { user: true }, attachments: true },
+      relations: { assignees: { user: true } },
       order: { index: 'DESC' },
+    });
+
+    const favoriteRecord = this.favorite.repository.findOne({ where: { userId, todolistId: id, isActive: true } });
+
+    const statusRecords = this.status.repository.find({
+      select: ['id', 'name', 'color', 'index'],
+      where: { todolistId: id, isActive: true },
+      order: { index: 'ASC' },
+    });
+
+    const memberRecords = this.member.repository.find({
+      select: ['todolistId', 'isActive'],
+      where: { todolistId: id, isActive: true },
+      relations: { user: true },
+    });
+
+    const promises = await Promise.all([todolistRecord, taskRecords, favoriteRecord, statusRecords, memberRecords]);
+
+    const todolist = promises[0];
+    const tasks = promises[1];
+    const favorite = Boolean(promises[2]);
+    const status = promises[3];
+    const members = promises[4].map(({ user }) => ({ id: user.id, name: user.name, email: user.email }));
+
+    tasks.forEach((e) => {
+      e.assignees = e.assignees.filter((e) => e.isActive);
+    });
+    status.forEach((e, idx) => {
+      e.tasks = tasks.filter((e) => e.statusId == status[idx].id);
+    });
+
+    if (todolist.visibility === this.visibilityList.private && userId !== todolist.userId)
+      throw new MethodNotAllowedException('Private list, you are not owner to view this');
+
+    return { ...todolist, favorite, status, members, tasks };
+  }
+
+  async getOneKanban({ id, userId }: ITodolistGetOne) {
+    if (!defineAll(id, userId)) throw new BadRequestException('Todolist getOne Err param');
+
+    const todolistRecord = this.repository.findOne({
+      select: ['id', 'name', 'userId', 'visibility'],
+      where: { id, isActive: true },
+    });
+
+    const taskRecords = this.task.repository.find({
+      select: ['id', 'name', 'isDone', 'statusId', 'index', 'priority', 'createdDate', 'indexColumn'],
+      where: { todolistId: id, isActive: true },
+      relations: { assignees: { user: true }, attachments: true },
+      order: { indexColumn: 'ASC' },
     });
 
     const favoriteRecord = this.favorite.repository.findOne({ where: { userId, todolistId: id, isActive: true } });
