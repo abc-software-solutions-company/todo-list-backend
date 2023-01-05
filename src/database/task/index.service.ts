7eb29d1f1fd40b4ae5b19de25c31e851fc87b692
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-05 11:14:37 +0700

    refactor

--- src/database/task/index.service.ts
+++ src/database/task/index.service.ts
@@ -60,15 +60,13 @@ export class TaskService {
   }
 
   async create(param: ITaskCreate) {
-    const { todolistId, userId, statusId: statusIdParam } = param;
+    const { todolistId, userId, statusId } = param;
     if (!defineAll(param)) throw new BadRequestException('Create Task Error Param');
 
-    const { index, statusId } = await this.createHelper({ todolistId, userId, statusId: statusIdParam });
+    const { index, indexColumn } = await this.createHelper({ todolistId, userId, statusId });
     const id = v4();
 
-    let user: Task;
-    if (!statusIdParam) user = this.repository.create({ id, ...param, index, statusId });
-    else user = this.repository.create({ id, ...param, index, statusId: statusIdParam });
+    const user = this.repository.create({ id, ...param, index, indexColumn, statusId });
 
     return this.repository.save(user);
   }
@@ -302,17 +300,15 @@ export class TaskService {
       relations: { status: true },
     });
 
-    const status = this.status.repository.find({ where: { todolistId }, order: { index: 'ASC' } });
-
-    const promises = await Promise.all([todolist, tasksLength, status, tasksLengthByStatus]);
+    const promises = await Promise.all([todolist, tasksLength, tasksLengthByStatus]);
 
     const { visibility, userId } = promises[0];
 
     if (visibility !== this.todolist.visibilityList.public && userId !== TaskUserId)
       throw new MethodNotAllowedException();
 
-    const indexColumn = (promises[3] + 1) * this.indexStep;
+    const indexColumn = (promises[2] + 1) * this.indexStep;
 
-    return { index: (promises[1] + 1) * this.indexStep, indexColumn, statusId: promises[2][0].id };
+    return { index: (promises[1] + 1) * this.indexStep, indexColumn };
   }
 }
