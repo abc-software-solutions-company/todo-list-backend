a8176dcfc1356e53feb9862691fd62f6ece90141
LeNhi-inter <95989630+NhiLe11@users.noreply.github.com>
2023-03-23 08:35:05 +0700

    NOTIFICATION-12: Add mark all as read for notification

--- src/database/notification/index.controller.ts
+++ src/database/notification/index.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SkipThrottle } from '@nestjs/throttler';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -23,6 +23,14 @@ export class NotificationController {
   @Patch(':id')
   @SkipThrottle()
   async update(@Param('id') id: number) {
-    return this.service.update({ id });
+    return this.service.updateOne({ id });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+  @SkipThrottle()
+  async updateAll(@Req() request: IRequest) {
+    const { id } = request.user;
+    return this.service.UpdateAll(id);
   }
 }
