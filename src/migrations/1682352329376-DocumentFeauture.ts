1e60f83e50eec7a62fe4ca7cd01d1604cb34aa28
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-04-24 23:17:52 +0700

    Release-24-3-2023-Fix-Migration-IsFeauture-property (#143)

--- src/migrations/1682352329376-DocumentFeauture.ts
+++ src/migrations/1682352329376-DocumentFeauture.ts
@@ -5,18 +5,9 @@ export class DocumentFeauture1682352329376 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "todolist"."task" ADD "isFeature" boolean NOT NULL DEFAULT false`);
-    await queryRunner.query(
-      `ALTER TABLE "todolist"."document" ADD CONSTRAINT "FK_0f10c2404755fd85c4cfa5f61be" FOREIGN KEY ("todolistId") REFERENCES "todolist"."todolist"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "todolist"."document" ADD CONSTRAINT "FK_4c4ae8a7a98116d84d0ecb087b9" FOREIGN KEY ("parentId") REFERENCES "todolist"."document"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "todolist"."document" DROP CONSTRAINT "FK_4c4ae8a7a98116d84d0ecb087b9"`);
-    await queryRunner.query(`ALTER TABLE "todolist"."document" DROP CONSTRAINT "FK_0f10c2404755fd85c4cfa5f61be"`);
     await queryRunner.query(`ALTER TABLE "todolist"."task" DROP COLUMN "isFeature"`);
-    await queryRunner.query(`DROP TABLE "todolist"."document"`);
   }
 }
