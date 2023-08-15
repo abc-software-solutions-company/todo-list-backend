aa2224dfc37c886764a2274a06ffc168dafa36c2
Phuoc Huu <a000014@abcsoftwarecompany.com>
2023-08-15 16:36:55 +0700

    TD Fix migration file

--- src/migrations/1692063961596-changeFavoriteToNumber.ts
+++ src/migrations/1692063961596-changeFavoriteToNumber.ts
@@ -4,12 +4,12 @@ export class ChangeFavoriteToNumber1692063961596 implements MigrationInterface {
   name = 'ChangeFavoriteToNumber1692063961596';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "document" DROP COLUMN "favorite"`);
-    await queryRunner.query(`ALTER TABLE "document" ADD "favorite" integer`);
+    await queryRunner.query(`ALTER TABLE "todolist"."document" DROP COLUMN "favorite"`);
+    await queryRunner.query(`ALTER TABLE "todolist"."document" ADD "favorite" integer`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "document" DROP COLUMN "favorite"`);
-    await queryRunner.query(`ALTER TABLE "attachment" DROP COLUMN "type"`);
+    await queryRunner.query(`ALTER TABLE "todolist"."document" DROP COLUMN "favorite"`);
+    await queryRunner.query(`ALTER TABLE "todolist"."document" ADD "favorite" boolean NOT NULL DEFAULT false`);
   }
 }
