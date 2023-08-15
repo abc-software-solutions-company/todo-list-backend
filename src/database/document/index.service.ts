fcf60a350e1a4172c76e001fb24f0bb2c7878a4d
Nhi Lê <120070400+nhile115@users.noreply.github.com>
2023-08-15 07:23:35 +0700

    Update index.service.ts

--- src/database/document/index.service.ts
+++ src/database/document/index.service.ts
@@ -30,10 +30,11 @@ export class DocumentService {
 
   async handleFavorite(documentId) {
     const result = await this.repository.findOneBy({ id: documentId });
-    const lastDocument = await this.repository.find({
-      where: { favorite: Not(IsNull()), todolistId: result.todolistId },
+    const lastDocument = await this.repository.findOne({
+      where: { todolistId: result.todolistId, favorite: Not(IsNull()) },
+      order: { favorite: 'DESC' },
     });
-    if (result.favorite === null) result.favorite = result.favorite = lastDocument ? lastDocument.length + 1 : 1;
+    if (result.favorite === null) result.favorite = result.favorite = lastDocument ? lastDocument.favorite + 1 : 1;
     else result.favorite = null;
     return this.repository.save(result);
   }
@@ -50,10 +51,19 @@ export class DocumentService {
   async getDocumentsFavorite(todolistId: string): Promise<Document[]> {
     const documents = await this.repository.find({
       where: { todolistId, isActive: true, favorite: Not(IsNull()) },
-      order: { favorite: 'DESC' },
     });
-    const tree = this.buildTree(documents, null);
-    return tree;
+
+    const tree = this.buildTree(await this.repository.find({ where: { todolistId, isActive: true } }), null);
+
+    const favoriteHasChildIds = new Set(tree.filter((item) => item.children).map((item) => item.id));
+
+    const filteredDocuments = documents.filter((doc) => !favoriteHasChildIds.has(doc.id));
+
+    const mergedDocuments = [...tree.filter((item) => item.children), ...filteredDocuments];
+
+    mergedDocuments.sort((a, b) => b.favorite - a.favorite);
+
+    return mergedDocuments;
   }
 
   private buildTree(documents: Document[], parentId: string): Document[] {
